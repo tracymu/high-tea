928c72598d507eecfdc751d1b5616654f6fe7080
tracymu <tracy.musung@moomumedia.com>
2013-10-02 19:21:35 +1000

    removed extraneous money methods

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,22 +1,11 @@
 class Visit < ActiveRecord::Base
-
-	
 	monetize :price_cents
-
-
-	before_create :set_defaults
 
 	belongs_to :admin
 
+	before_create :set_defaults
 
-	CURRENCIES = %w(AUD USD EUR CNY HKD NZD AED INR GBP AED RUB)
-
-	composed_of :price,
-              :class_name => "Money",
-              :mapping => [%w(price_cents cents), %w(price_currency currency_as_string)],
-              :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
-              :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
-
+	# CURRENCIES = %w(AUD USD EUR CNY HKD NZD AED INR GBP AED RUB)
 
 	def total_score
 		ambience + tea + sweets + savoury + service + bonus + scones
@@ -25,7 +14,6 @@ class Visit < ActiveRecord::Base
 	def formatted_date	
 		self.date.strftime "%m-%Y"
 	end
-
 
 private 
 
@@ -38,13 +26,6 @@ private
 		bonus = 0
 		scones = 0
 	end
-
-
-	def price
-		# Money.new price_cents, price_currency
-
-	end
-
 end
 
 
